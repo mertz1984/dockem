3612159b552e202d5d2c01ee5ac5febc4bc3cc0c
mertz1984 <martin.ertz@gmail.com>
2022-02-19 21:28:13 -0600

    ignore the .angular cache. add theme and fix project dependencies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { Tooltip, Toast, Popover } from 'bootstrap';
 import { AppComponent } from './app.component';
 import { PenaltyDisplayComponent } from './penalty-display/penalty-display.component';
+import { PenaltyAddComponent } from './penalty-add/penalty-add.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, PenaltyDisplayComponent ],
+  declarations: [ AppComponent, PenaltyDisplayComponent, PenaltyAddComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/penalty-add/penalty-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-penalty-add',
+  templateUrl: './penalty-add.component.html',
+  styleUrls: ['./penalty-add.component.css']
+})
+export class PenaltyAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
